cc6b694e9e2cfb83a234fd994e96ae6e323e3c55
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-06-14 01:45:07 +0900

    Update TokenProvider.java

--- src/main/java/com/example/todo/auth/TokenProvider.java
+++ src/main/java/com/example/todo/auth/TokenProvider.java
@@ -21,7 +21,7 @@ import java.util.HashSet;
 @Slf4j
 public class TokenProvider {
 
-    // 토큰을 발급하려면 서명이 필요 -> 서명에 사용할 값이 필요 (512비트(64바이트) 이상의 랜덤 문자열이 필요함 -> 해킹위험예방)
+    // 토큰을 발급하려면 서명(시그니처)이 필요 -> 서명에 사용할 값이 필요 (512비트(64바이트) 이상의 랜덤 문자열이 필요함 -> 해킹위험예방)
     @Value("${jwt.secret}")
     private String SECRET_KEY; // 서명은 노출되면 안되니까 yml에 설정
 
@@ -62,13 +62,13 @@ public class TokenProvider {
 
         // 토큰 생성
         return Jwts.builder()
-                // token header에 들어갈 서명
+                // ↓ token header에 들어갈 서명
                 .signWith(
-                        // SECRET_KEY를 암호화
+                        // 내 서명(SECRET_KEY)를 암호화
                         Keys.hmacShaKeyFor(SECRET_KEY.getBytes())
                         , SignatureAlgorithm.HS512
                 )
-                // token payload(body같음)에 들어갈 '클레임' 설정 (자주쓰는 것은 인텔리제이에 있음)
+                // ↓ token payload(body같음)에 들어갈 '클레임' 설정 (자주쓰는 것은 인텔리제이에 있음)
                 .setClaims(claims) // 커스텀으로 더 넣고 싶은거 (추가클레임) * 주의사항 * 추가클레임은 먼저 설정해야함
                 .setIssuer("바닐라겅듀") // iss: 발급자 정보 (회사이름, 서비스이름)
                 .setIssuedAt(new Date()) // iat: 토큰 발급시간
@@ -85,22 +85,21 @@ public class TokenProvider {
      */
     public TokenUserInfo validatedAndGetTokenUserInfo(String token) {
 
-        // 디코딩(암호화 해제)
+        // parserBuilder로 암호화 해제(디코딩)
         Claims claims = Jwts.parserBuilder()
-                // 토큰 발급자의 발급 당시의 서명을 넣어줌
-                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes())) // 서명을 비교하기 위해서 토큰 발급자의 발급 당시의 서명을 넣어줌
                 .build()
-                // 여기까지가 서명위조 검사작업 임 (위조된 경우, 예외가 발생함)
-                // 위조가 되지 않은 경우, 페이로드(클레임)을 리턴 -> 페이로드의 클레임을 파싱함 -> 클레임 리턴
-                .parseClaimsJws(token)
-                .getBody();
+                // 여기까지가 서명위조 검사작업임 (위조된 경우, 예외가 발생함)
+                // 위조가 되지 않은 경우, 페이로드(클레임)을 리턴
+                .parseClaimsJws(token) // 페이로드 안에서 클레임을 파싱해서 제이슨 가져오기
+                .getBody(); // 토큰바디안에 있는 클레임 가져오기
 
-        log.info("claims: {}", claims);
+        log.info("claims: {}", claims); // claims의 정체는 72~76번 라인! // 이걸 모아서 ↓ 예쁘게 포장해서 서버로 주기!
 
         return TokenUserInfo.builder()
-                .userId(claims.getSubject()) // id는 Subject안에 넣었음
-                .email(claims.get("email", String.class)) //map으로 들어갔으니까, String클래스로 꺼내야함
-                .role(Role.valueOf(claims.get("role", String.class))) //map으로 들어갔으니까, String클래스로 꺼내야함 -> 근데 enum이니까 한번 더 변환 필요
+                .userId(claims.getSubject()) // 토큰 만들때 id는 Subject안에 넣었었음
+                .email(claims.get("email", String.class)) //map에서 Object로 들어갔으니까, String클래스로 꺼내야함
+                .role(Role.valueOf(claims.get("role", String.class))) //map에서 Object로 들어갔으니까, String클래스로 꺼내야함 -> 근데 enum이니까 한번 더 enum으로 변환 필요
                 .build();
     }
 
